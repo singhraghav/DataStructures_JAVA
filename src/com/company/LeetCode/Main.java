f11089d65e9b48598d71622cd890fd1807187e24
singhraghav <raghavsingh31@outlook.com>
2021-09-10 18:40:58 +0530

    leet code day 9

--- src/com/company/LeetCode/Main.java
+++ src/com/company/LeetCode/Main.java
@@ -1,6 +1,7 @@
 package com.company.LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -36,6 +37,38 @@ public class Main {
         }
        return root;
     }
+
+    public int[][] updateMatrix(int[][] mat) {
+        int[][] result = new int[mat.length][];
+        for(int i =0; i < mat.length; i++) {
+            result[i] = new int[mat[i].length];
+            Arrays.fill(result[i], Integer.MAX_VALUE);
+        }
+
+        for(int i =0; i < mat.length; i++) {
+            for(int j =0; j < mat[i].length; j++) {
+                if(mat[i][j] == 0)
+                    result[i][j] = 0;
+                else {
+                    if(i > 0)
+                        result[i][j] = Math.min(result[i][j], result[i-1][j] + 1);
+                    if(j > 0)
+                        result[i][j] = Math.min(result[i][j], result[i][j - 1] + 1);
+                }
+            }
+        }
+
+        for(int i = mat.length - 1; i >= 0; i--) {
+            for(int j =mat[i].length - 1; j >=0 ; j--) {
+                if(i < mat.length - 1)
+                    result[i][j] = Math.min(result[i][j], result[i+1][j] + 1);
+                if(j < mat[i].length - 1)
+                    result[i][j] = Math.min(result[i][j], result[i][j+1] + 1);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
     }
